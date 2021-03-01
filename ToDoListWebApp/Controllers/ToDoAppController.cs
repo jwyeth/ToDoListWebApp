930a9a1baa7ffea3e695d0bbd179841201463ff7
jwyeth <jwyeth@hotmail.com>
2021-02-28 22:23:04 -0500

    initial commit

---
+++ ToDoListWebApp/Controllers/ToDoAppController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using ToDoListWebApp.Models;
+
+namespace ToDoListWebApp.Controllers
+{
+    public class ToDoAppController : Controller
+    {
+        private readonly ToDoAppContext _toDoAppDB;
+
+        public ToDoAppController(ToDoAppContext toDoAppContext)
+        {
+            _toDoAppDB = toDoAppContext;
+        }
+
+        public IActionResult Index()
+        {
+            string loggedInUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            List<Tasks> taskList = _toDoAppDB.Tasks.ToList();
+            List<Tasks> userTaskList = taskList.Where(x => x.OwnerId == loggedInUserId).ToList();
+
+            return View(userTaskList);
+        }
+    }
+}
---
+++ ToDoListWebApp/Models/Tasks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoListWebApp.Models
+{
+    public partial class Tasks
+    {
+        public int TaskId { get; set; }
+        public string OwnerId { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool Completed { get; set; }
+
+        public virtual AspNetUsers Owner { get; set; }
+    }
+}
