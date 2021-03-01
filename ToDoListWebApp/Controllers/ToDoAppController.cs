8ee042b37a62f9fa5aca4c725b21839448aa011a
jwyeth <jwyeth@hotmail.com>
2021-03-01 09:15:56 -0500

    A lot of refactoring

--- ToDoListWebApp/Controllers/ToDoAppController.cs
+++ ToDoListWebApp/Controllers/ToDoAppController.cs
@@ -6,9 +6,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Security.Claims;
 using ToDoListWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ToDoListWebApp.Controllers
 {
+    [Authorize]
     public class ToDoAppController : Controller
     {
         private readonly ToDoAppContext _toDoAppDB;
@@ -17,16 +19,15 @@ namespace ToDoListWebApp.Controllers
         {
             _toDoAppDB = toDoAppContext;
         }
-
         public IActionResult Index()
         {
             string loggedInUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
-
-            List<Tasks> taskList = _toDoAppDB.Tasks.ToList();
-            List<Tasks> userTaskList = taskList.Where(x => x.OwnerId == loggedInUserId).ToList();
+            List<Tasks> userTaskList = _toDoAppDB.Tasks.Where(x => x.OwnerId == loggedInUserId).ToList();
 
             return View(userTaskList);
         }
+
+        [Authorize]
 
         public IActionResult AddTask()
         {
@@ -42,7 +43,42 @@ namespace ToDoListWebApp.Controllers
             _toDoAppDB.Tasks.Add(task);
             _toDoAppDB.SaveChanges();
             return RedirectToAction("Index");
+        }
 
+        public IActionResult ViewTasks()
+        {
+            string loggedInUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            List<Tasks> userTaskList = _toDoAppDB.Tasks.Where(x => x.OwnerId == loggedInUserId).ToList();
+
+            return View(userTaskList);
+        }
+
+        public IActionResult MarkComplete(int id)
+        {
+            Tasks task = _toDoAppDB.Tasks.Find(id);
+            task.Completed = true;
+            _toDoAppDB.Tasks.Update(task);
+            _toDoAppDB.SaveChanges();
+            return RedirectToAction("Success", task);
+        }
+
+        public IActionResult Sucess(Tasks task)
+        {
+            return View(task);
+        }
+
+        public IActionResult MarkIncomplete(int id)
+        {
+            Tasks task = _toDoAppDB.Tasks.Find(id);
+            task.Completed = false;
+            _toDoAppDB.Tasks.Update(task);
+            _toDoAppDB.SaveChanges();
+            return RedirectToAction("Success", task);
+        }
+
+        public IActionResult Error()
+        {
+            return View();
         }
     }
 }
